5909d4f92ec69c7ac6209a3e78bfe2dc8e7cf0cf
toiki <tuiboku1128@gmail.com>
2023-06-10 15:58:23 +0900

    不要なコメント削除

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  tabWidth: 2, // インデントのスペースの数
-  useTabs: false, // インデントにタブを使用するかどうか
-  semi: true, // 文末にセミコロンを付けるかどうか
-  singleQuote: true, // シングルクォートを使用するかどうか
-  trailingComma: 'es5', // 末尾のカンマのスタイル（es5、none、allのいずれか）
-  bracketSpacing: true, // オブジェクトリテラルの中のスペースの有無
-  arrowParens: 'always', // アロー関数の引数を常に括弧で囲むかどうか
+  tabWidth: 2, 
+  useTabs: false, 
+  semi: true, 
+  singleQuote: true, 
+  trailingComma: 'all', 
+  bracketSpacing: true, 
+  arrowParens: 'always', 
 };
